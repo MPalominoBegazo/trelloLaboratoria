7c17c3c6a3e4830944afc64efaeaa1608ecddd5a
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-08-09 02:10:51 -0700

    update2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ var btnDesplegar = document.getElementById("addList");
 var lista = document.getElementById("lista");
 var resultado = document.getElementById("desplegar");
 var btnListar = document.getElementById("buttonList");
+var imgClose = document.getElementById("close");
 
 //OBJETO
 function Tarea(nameTask, taskID){
@@ -35,19 +36,37 @@ function printHTML (html){
 }
 //BOTON PARA MOSTRAR AÑADIR TAREA
 btnDesplegar.onclick = function (){
-   // btnListar.style.display = "none";
     resultado.style.display = "block";
-
+    //btnListar.style.display = "none";
 }
 
 //BOTON AÑADIR TAREA
 btnAdd.onclick = function (){
+    var crearLista = document.getElementById("resultado");
     id++;
     var inputTask = document.getElementById("task").value;
     var task = new Tarea(inputTask,id);
     tareas.push(task);
-    printHTML(task.toHTML());
-    printHTML(mergeHTML());
+    //creamos un DIV para la lista creada
+    var divLista = document.createElement("div");
+    divLista.setAttribute("id",inputTask);
+
+    //creamos los elementos del Div
+    var par = document.createElement("p");
+    par.setAttribute("class", "ParrafoLista");
+    par.setAttribute("textContent",inputTask);
+
+    
+    resultado.appendChild(divLista);
     document.getElementById("task").value = "";
 
 }   
+
+//Eliminar
+imgClose.onclick = function(){
+    resultado.style.display = "none";
+
+}
+
+
+
